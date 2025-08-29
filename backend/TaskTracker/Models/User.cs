e3ac72834417127f249e3521cac8f1a139a82f8d
markomitrovicc <mitrovicmarkoit@gmail.com>
2025-08-30 01:51:50 +0200

    Added a Foreign key to Task model. Created User model.

---
+++ backend/TaskTracker/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace TaskTracker.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Username { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string PasswordHash { get; set; } = null!;
+        public List<Task> Tasks { get; set; } = new();
+    }
+}
