22220ee08ea0ad852a4a6b5940ee5060425f10f1
markomitrovicc <mitrovicmarkoit@gmail.com>
2025-08-30 01:26:30 +0200

    Added base Task model

---
+++ backend/TaskTracker/Models/Task.cs
@@ -0,0 +1,12 @@
+﻿namespace TaskTracker.Models
+{
+    public class Task
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? CompletedAt { get; set; }
+    }
+}
